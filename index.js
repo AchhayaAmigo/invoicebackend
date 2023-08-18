4f9b9f9bc12713093f5411d940cd976145d829b4
AchhayaAmigo <achhaya@amigo.gg>
2023-08-18 14:49:33 +0530

    Added Middlewares

--- index.js
+++ index.js
@@ -1,11 +1,79 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3001;
+const cors = require("cors");
+const { logger } = require("./middlewares/logEvents");
+const errorHandler = require("./middlewares/errorHandler");
+const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = [
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Testing Home Route");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${3000}!`);
+const handleSignin = (req, res) => {
+    res.send("Login");
+}
+
+// write handleSignUp function same as login function
+const handleSignUp = (req, res) => {
+    res.send("SignUp");
+}
+
+const handleBills = (req, res) => {
+    res.send("All Bills");
+}
+
+const handleBillByID = (req, res) => {
+    res.send("Bill by ID");
+}
+
+const handleDetails = (req, res) => {
+    res.send("Company Details");
+}
+
+app.post("/signin", handleSignin);
+app.post("/signup", handleSignUp);
+app.get("/bills", handleBills);
+app.get("/bill/:billId", handleBillByID);
+app.get("/details", handleDetails);
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+  console.log("Press Ctrl+C to quit.");
 });
