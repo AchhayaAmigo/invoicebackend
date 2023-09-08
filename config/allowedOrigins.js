13c6cbb484e865ead932cc05030f286a4bce5460
AchhayaAmigo <achhaya@amigo.gg>
2023-09-08 17:19:35 +0530

    added origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["https://inovie-front.vercel.app"];
+const allowedOrigins = ["https://inovie-front.vercel.app", "https://walrus-app-c6geq.ondigitalocean.app/"];
 
 module.exports = allowedOrigins;
